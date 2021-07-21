6476e11d3b7e512f7224c8577f9098ffc925f4f4
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-21 13:21:45 +0500

    Add work with TextView from the code

--- app/src/main/java/com/github/marfikus/helloworld/MainActivity.kt
+++ app/src/main/java/com/github/marfikus/helloworld/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.github.marfikus.helloworld
 
+import android.content.res.Resources
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.annotation.ColorRes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val titleTextView = findViewById<TextView>(R.id.titleTextView)
+        val string = "New text"
+        titleTextView.text = string
+//        titleTextView.setTextColor(Color.parseColor("#FFFF00"))
+        titleTextView.setColor(R.color.black)
+    }
+
+    fun TextView.setColor(@ColorRes colorResId: Int, theme: Resources.Theme? = null) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            setTextColor(resources.getColor(colorResId, theme))
+        } else {
+            setTextColor(resources.getColor(colorResId))
+        }
     }
 }
