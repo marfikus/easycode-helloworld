3c5b7f27e678b46f437be829269526db5852baef
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-21 14:25:39 +0500

    Add clickableSpans

--- app/src/main/java/com/github/marfikus/helloworld/MainActivity.kt
+++ app/src/main/java/com/github/marfikus/helloworld/MainActivity.kt
@@ -1,24 +1,78 @@
 package com.github.marfikus.helloworld
 
 import android.content.res.Resources
+import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.View
 import android.widget.TextView
 import androidx.annotation.ColorRes
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val agreementTextView = findViewById<TextView>(R.id.agreementTextView)
-        val string = "New text"
-        agreementTextView.text = string
+        val agreementTextView: TextView = findViewById(R.id.agreementTextView)
+
+        val fullText = getString(R.string.agreement_fully_text)
+        val confidential = getString(R.string.confidential_info)
+        val policy = getString(R.string.privacy_policy)
+        val spannableString = SpannableString(fullText)
+
+        val confidentialClickable = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                Snackbar.make(widget, getString(R.string.go_to_link_1), Snackbar.LENGTH_SHORT).show()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = true
+                ds.color = Color.parseColor("#FF0000")
+            }
+        }
+
+        val policyClickable = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                Snackbar.make(widget, getString(R.string.go_to_link_2), Snackbar.LENGTH_SHORT).show()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = true
+                ds.color = Color.parseColor("#FF0000")
+            }
+        }
+
+        spannableString.setSpan(
+            confidentialClickable,
+            fullText.indexOf(confidential),
+            fullText.indexOf(confidential) + confidential.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        spannableString.setSpan(
+            policyClickable,
+            fullText.indexOf(policy),
+            fullText.indexOf(policy) + policy.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        agreementTextView.run {
+            text = spannableString
+            movementMethod = LinkMovementMethod.getInstance()
+            highlightColor = Color.TRANSPARENT
+        }
+
 //        agreementTextView.setTextColor(Color.parseColor("#FFFF00"))
-        agreementTextView.setColor(R.color.black)
-        agreementTextView.visibility = View.INVISIBLE
+//        agreementTextView.setColor(R.color.black)
     }
 
     fun TextView.setColor(@ColorRes colorResId: Int, theme: Resources.Theme? = null) {
