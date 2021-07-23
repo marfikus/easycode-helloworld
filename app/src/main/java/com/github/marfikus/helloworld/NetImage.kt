7cc76e4c597ee0a64413f73c47c63ef51392eb05
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-23 06:50:05 +0500

    Add class NetImage

---
+++ app/src/main/java/com/github/marfikus/helloworld/NetImage.kt
@@ -0,0 +1,25 @@
+package com.github.marfikus.helloworld
+
+import android.graphics.BitmapFactory
+import java.lang.Exception
+import java.net.URL
+
+class NetImage(
+    private val url: String,
+    private val callback: ImageCallback
+) : Thread() {
+
+    override fun run() {
+        super.run()
+        try {
+            val connection = URL(url).openConnection()
+            connection.doInput = true
+            connection.connect()
+            connection.getInputStream().use {
+                callback.success(BitmapFactory.decodeStream(it))
+            }
+        } catch (e: Exception) {
+            callback.failed()
+        }
+    }
+}
