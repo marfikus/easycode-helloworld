68af48dc41ea862d1e454ffd1f53d5657634c24c
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-26 22:24:25 +0500

    Add class MyApplication

---
+++ app/src/main/java/com/github/marfikus/helloworld/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.github.marfikus.helloworld
+
+import android.app.Application
+
+class MyApplication : Application() {
+
+    lateinit var viewModel: ViewModel
+
+    override fun onCreate() {
+        super.onCreate()
+        viewModel = ViewModel(Model())
+    }
+}
