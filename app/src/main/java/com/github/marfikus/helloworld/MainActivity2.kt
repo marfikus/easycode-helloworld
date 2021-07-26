a4bf2d55defcd6e1b73edcea27ef1c1a7a0598cb
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-26 22:39:00 +0500

    Add main logic

--- app/src/main/java/com/github/marfikus/helloworld/MainActivity2.kt
+++ app/src/main/java/com/github/marfikus/helloworld/MainActivity2.kt
@@ -5,20 +5,26 @@ import android.os.Bundle
 import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var viewModel: ViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        viewModel = (application as MyApplication).viewModel
         val textView = findViewById<TextView>(R.id.textView)
-
         val observable = TextObservable()
         observable.observe(object : TextCallback {
             override fun updateText(str: String) = runOnUiThread {
                 textView.text = str
             }
         })
+        viewModel.init(observable)
+    }
 
-        val viewModel = ViewModel(observable)
-        viewModel.init()
+    override fun onDestroy() {
+        viewModel.clear()
+        super.onDestroy()
     }
 }
