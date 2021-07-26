aab5061b35d1fc46c25bbfb78a7b878ab7cc5aef
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-26 18:08:17 +0500

    Add main logic to activity

--- app/src/main/java/com/github/marfikus/helloworld/MainActivity2.kt
+++ app/src/main/java/com/github/marfikus/helloworld/MainActivity2.kt
@@ -2,10 +2,23 @@ package com.github.marfikus.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        val observable = TextObservable()
+        observable.observe(object : TextCallback {
+            override fun updateText(str: String) = runOnUiThread {
+                textView.text = str
+            }
+        })
+
+        val viewModel = ViewModel(observable)
+        viewModel.init()
     }
 }
