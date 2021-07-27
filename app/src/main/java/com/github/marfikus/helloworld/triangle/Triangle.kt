8774f088bcecbc3e2c696e788b768754ac591f34
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-27 14:14:38 +0500

    Fix Triangle class, add more tests

--- app/src/main/java/com/github/marfikus/helloworld/triangle/Triangle.kt
+++ app/src/main/java/com/github/marfikus/helloworld/triangle/Triangle.kt
@@ -4,6 +4,15 @@ class Triangle(private val sideA: Int,
                private val sideB: Int,
                private val sideC: Int) {
 
+    init {
+        if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
+            throw IllegalArgumentException("triangle sides cannot be non-positive")
+        }
+        if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA) {
+            throw IllegalArgumentException("invalid triangle with sides: $sideA $sideB $sideC")
+        }
+    }
+
     fun isRightTriangle(): Boolean {
         return sideA.square() + sideB.square() == sideC.square() ||
                 sideA.square() + sideC.square() == sideB.square() ||
