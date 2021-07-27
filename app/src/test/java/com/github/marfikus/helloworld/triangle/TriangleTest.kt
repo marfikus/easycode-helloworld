c5ce8106d1a3a0485159521d7ec57e91f5b5adad
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-27 13:33:20 +0500

    Add first tests

--- app/src/test/java/com/github/marfikus/helloworld/triangle/TriangleTest.kt
+++ app/src/test/java/com/github/marfikus/helloworld/triangle/TriangleTest.kt
@@ -1,5 +1,24 @@
 package com.github.marfikus.helloworld.triangle
 
 import org.junit.Assert.*
+import org.junit.Test
 
-class TriangleTest
+class TriangleTest {
+
+    @Test
+    fun test_valid_data() {
+        val triangle = Triangle(3, 4, 5)
+        val actual = triangle.isRightTriangle()
+        val expected = true
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_negative_data() {
+        val triangle = Triangle(-3, -4, -5)
+        val actual = triangle.isRightTriangle()
+        val expected = false
+        assertEquals(expected, actual)
+    }
+
+}
